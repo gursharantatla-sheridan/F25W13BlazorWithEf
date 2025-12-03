b81d259b5307e3fcad7de73e8e5e9e23949085cf
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-12-03 13:28:28 -0500

    dbcontext class

--- F25W13BlazorWithEf/Data/AppDbContext.cs
+++ F25W13BlazorWithEf/Data/AppDbContext.cs
@@ -1,16 +1,26 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using F25W13BlazorWithEf.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace F25W13BlazorWithEf.Data
 {
     public class AppDbContext : DbContext
     {
         // constructor
-
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        { 
+        }
 
         // entity sets
-
+        public DbSet<Product> Products { get; set; }
 
         // data seeding
-
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product { Id = 1, Name = "Laptop", Price = 1000 },
+                new Product { Id = 2, Name = "Mobile", Price = 1500 },
+                new Product { Id = 3, Name = "Tablet", Price = 500 }
+            );
+        }
     }
 }
